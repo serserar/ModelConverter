9e682d31e72730f34c6fbc2a43a4e09ebaf0d7f6
Sergio Serrano <serserar@gmail.com>
2018-09-08 22:12:04 +0200

    remove generate data set. use script scripts/gendataset.sh

--- src/modelSampler.cpp
+++ src/modelSampler.cpp
@@ -14,17 +14,8 @@ void generateSamples(){
     modelSamplerApp.Run();
 }
 
-void generateDataSet(){
-    vector<string> modelsPath;
-    vector<string> skeletonsPath;
-    std::regex model_regex("(\\model_)");
-    std::regex skeleton_regex("(\\skeleton_)");
-    string sourcePath = "../samples/out";
-}
-
 int main(int argc, char** argv) {
     generateSamples();
-    //generateDataSet();
     return 0;
 }
 
